55c38bb7f7b89f8da36091681c84e7bcda2ac0ec
FloMeyer <flo@flomeyer.de>
2021-05-15 15:27:55 +0000

    Added init file

---
+++ netbox_qr/__init__.py
@@ -0,0 +1,17 @@
+from extras.plugins import PluginConfig
+
+class QRConfig(PluginConfig):
+    """Plugin configuration for the netbox_qr plugin."""
+
+    name = "netbox_qr"
+    verbose_name = "QR Code plugin for netbox."
+    version = "0.1.0"
+    author = "Florian Meyer"
+    description = "A netbox plugin for generating qr codes for specific pages."
+    base_url = "qr"
+    required_settings = []
+    default_settings = {}
+    caching_config = {}
+
+
+config = QRConfig  # pylint:disable=invalid-name
