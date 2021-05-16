0bd3c7163a16171aaa9e942d5d58a520894a3d3a
FloMeyer <flo@flomeyer.de>
2021-05-16 12:38:42 +0000

    ...

--- netbox_qr/template_content.py
+++ netbox_qr/template_content.py
@@ -1,14 +1,22 @@
 from extras.plugins import PluginTemplateExtension
 import segno
+#import io
 
 class DeviceContent(PluginTemplateExtension):
-    model = 'dcim.cable'
+    model = 'dcim.device'
 
     def right_page(self):
-        test = "Test"
-        qr = segno.make('Yellow Submarine')
+#        qr = segno.make('Yellow Submarine')
+#        buffer = io.BytesIO()
+#        qr.save(buffer, format="PNG")
+#        qr_b64 = "data:image/png;base64,"+base64.b64encode(buffer.getvalue()).decode("utf-8")
+# working
+#        qr = segno.make('Up Jumped the Devil').svg_data_uri()
+        url = self.context['request'].build_absolute_uri(self.context['object'].get_absolute_url())
+        qr = segno.make_qr(url).svg_data_uri(scale=10)
+
         return self.render('netbox_qr/device_qr.html', extra_context={
-            'image': qr,
+            'qr': qr,
         })
 
 template_extensions = [DeviceContent]
