36ccffb9daf3b8eb0211e1acdd7f15db2400a8ad
FloMeyer <flo@flomeyer.de>
2022-05-31 20:19:45 +0000

    modified version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name = 'netbox_qr',
-    version = '0.1.3',
+    version = '0.1.4',
     python_requires='>=3',
     license='Apache 2.0',
     description = 'A netbox plugin for generating qr codes for specific pages.',
